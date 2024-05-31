881ae0ecb9159d9285fb8231fb2c07872cd108ce
Skinopathy <skinopathyresearch@gmail.com>
2024-05-30 22:29:56 -0400

    first commit

---
+++ app/app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+
+st.markdown("""# This is a header
+## This is a sub header
+This is text
+- are
+- you
+- serious""")
+
+
+
+def fun(a, b):
+    '''docstring of the function'''
+    return a + b
+
+st.write(fun)
+
+
+df = pd.DataFrame({
+    'first column': list(range(1, 11)),
+    'second column': np.arange(10, 101, 10)
+})
+
+# this slider allows the user to select a number of lines
+# to display in the dataframe
+# the selected value is returned by st.slider
+line_count = st.slider('Select a line count', 1, 10, 3)
+
+# and used to select the displayed lines
+head_df = df.head(line_count)
+
+head_df
